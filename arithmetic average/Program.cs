4d2e539cc26ad7b0fcbf71477beb462bcc32d537
Malgorzata Konieczna <gola434@wp.pl>
2022-10-05 13:49:12 +0200

    Add project files.

---
+++ arithmetic average/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        float inputValue;
+        float suma = 0;
+        int i = 0;
+
+        do
+        {
+            Console.WriteLine("Insert the number: ");
+            inputValue = float.Parse(Console.ReadLine());
+            if (inputValue == 0)
+            {
+                break;
+            }
+            suma += inputValue;
+            i++;
+        } while (inputValue != 0);
+
+        Console.WriteLine("average it: {0}", Math.Round(suma / i, 2));
+    }
+}
